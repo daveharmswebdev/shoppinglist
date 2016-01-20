7e2c40132d1d919657e9f30f3dd1a9c5ed5ce597
daveharmswebdev <wdave.harms@gmail.com>
2016-01-20 14:53:37 -0600

    final version?

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   $('#addStore').click(function() {
     $('#addStore').hide();
     $('#storeText').show();
+    $('#storeText').focus();
   });
 
   $('#storeText').keydown(function(enter) {
@@ -13,13 +14,11 @@ $(document).ready(function() {
       var storenospace = store.replace(/\s+/g, '');
       $('#storeText').val('');
       $('.storelist').append('<div class=\'astore column full\'><h2>'+store+'</h2><ul id=\''+storenospace+'\' class=\'connectedSortable\'></ul><i id=\''+storenospace+'check\'class=\'fa fa-plus-circle fa-4x\'></i><input id=\'itemText\' class=\''+storenospace+'input\' type=\'text\' name=\'item\' value=\'\'></div>');
-      // $('ul').sortable({containment: 'parent',
-      //   tolerance: 'pointer', connectWith: '.connectedSortable'
-      // });
       $('ul').sortable({tolerance: 'pointer',connectWith: '.connectedSortable'});
       $('.stores').on('click', '#'+storenospace+'check', function() {
         $('.'+storenospace+'input').show();
         $('#'+storenospace+'check').hide();
+        $('.'+storenospace+'input').focus();
       });
       $('.stores').on('keydown','.'+storenospace+'input', function(enter) {
         if (enter.keyCode == 13) {
@@ -34,24 +33,17 @@ $(document).ready(function() {
       $('#'+storenospace).on('click', '.fa-minus-square', function() {
           $(this).parents('.item').remove();
       });
-
-      // here is the problem down below.
-
       $('#'+storenospace).on('click', '.'+storenospace+'.fa-check-square',function() {
         console.log('click '+$(this).text());
         $(this).parents('.item').toggleClass('check');
       });
-
-      // here is the problem up above.
-
     }
-
   });
 
-  function addItem(item) {
-    var gitem = $('input').val();
-    console.log(store+item);
-    // $('#'+selectStore).append("<li><div class=\"item uncheck\"><i class=\"fa fa-check-square\"></i><p>"+gitem+"</p><i class=\"fa fa-minus-square\"></i></div></li>");
-  }
+  // function addItem(item) {
+  //   var gitem = $('input').val();
+  //   console.log(store+item);
+  //   // $('#'+selectStore).append("<li><div class=\"item uncheck\"><i class=\"fa fa-check-square\"></i><p>"+gitem+"</p><i class=\"fa fa-minus-square\"></i></div></li>");
+  // }
 
 });
