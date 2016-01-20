abba5bf47f179545b342587bb65424b991b22d0f
daveharmswebdev <wdave.harms@gmail.com>
2016-01-19 21:53:55 -0600

    adding stores

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
 
   $('#addStore').click(function() {
-    console.log('addStore click');
     $('#addStore').hide();
     $('#storeText').show();
   });
+
+  $('#storeText').keydown(function(enter) {
+    if (enter.keyCode == 13) {
+      $('#addStore').show();
+      $('#storeText').hide();
+      var store = $('#storeText').val();
+      var storenospace = store.replace(/\s+/g, '');
+      $('#storeText').val('');
+      $('.storelist').append('<div class=\'astore column full\'><h2>'+store+'</h2><ul id=\''+storenospace+'\' class=\'connectedSortable\'></ul><i id=\''+storenospace+'check\'class=\'fa fa-plus-circle fa-4x\'></i><input id=\'itemText\' type=\'text\' name=\'item\' value=\'\'></div>');
+      $('.stores').on('click', '#'+storenospace+'check', function() {
+        console.log(this);
+      });
+    }
+
+  });
+
+
 
 });
