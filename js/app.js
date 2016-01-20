0beeeec001c573bbf6bd5ac0b4eeff701d0de1c5
daveharmswebdev <wdave.harms@gmail.com>
2016-01-19 22:12:26 -0600

    adding stores good stopping point

--- js/app.js
+++ js/app.js
@@ -12,9 +12,10 @@ $(document).ready(function() {
       var store = $('#storeText').val();
       var storenospace = store.replace(/\s+/g, '');
       $('#storeText').val('');
-      $('.storelist').append('<div class=\'astore column full\'><h2>'+store+'</h2><ul id=\''+storenospace+'\' class=\'connectedSortable\'></ul><i id=\''+storenospace+'check\'class=\'fa fa-plus-circle fa-4x\'></i><input id=\'itemText\' type=\'text\' name=\'item\' value=\'\'></div>');
+      $('.storelist').append('<div class=\'astore column full\'><h2>'+store+'</h2><ul id=\''+storenospace+'\' class=\'connectedSortable\'></ul><i id=\''+storenospace+'check\'class=\'fa fa-plus-circle fa-4x\'></i><input id=\'itemText\' class=\''+storenospace+'input\' type=\'text\' name=\'item\' value=\'\'></div>');
       $('.stores').on('click', '#'+storenospace+'check', function() {
-        console.log(this);
+        $('.'+storenospace+'input').show();
+        $('#'+storenospace+'check').hide();
       });
     }
 
