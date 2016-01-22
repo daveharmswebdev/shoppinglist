cb4a400316819beb78dd1f08a0b62b63932bd6c2
Dave <wdave.harms@gmail.com>
2016-01-22 03:33:09 +0000

    js changes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
-
 // global variables
 var store,
     storenospace;
     
 // functions
-
   function makestore(store) {
-    var storenospace = store.replace(/\s+/g, '');
-    $('.storelist').append('<div class=\'astore column full\'><h2>'+store+'</h2><ul id=\''+storenospace+'\' class=\'connectedSortable\'></ul><i id=\''+storenospace+'check\'class=\'fa fa-plus-circle fa-4x\'></i><input id=\'itemText\' class=\''+storenospace+'input\' type=\'text\' name=\'item\' value=\'\'><i id=\''+storenospace+'faenter\' class=\"fa fa-sign-in fa-4x\"></i></div>');
+    storenospace = store.replace(/\s+/g, '');
+    $('.storelist').append(getItem(store,storenospace));
     $('ul').sortable({tolerance: 'pointer',connectWith: '.connectedSortable'});
   }
   
@@ -16,8 +14,22 @@ var store,
     var InputBox = '<input id=\"storeText\" type=\"text\" name=\"item\" value=\"\"><i id=\'fa-enter\' class=\"fa fa-sign-in fa-4x\"></i>';
     return InputBox;
   }
-// events
+  
+  function getItem(store, storenospace) {
+    var Item = '<div class=\'storeStem column full\'><h2>'+store+'</h2><ul id=\''+storenospace+'\' class=\'connectedSortable\'></ul><i id=\''+storenospace+'check\'class=\'fa fa-plus-circle fa-4x\'></div>';
+    return Item;
+  }
+  
+// event handlers
+  $('#addStoreStem').on('click','#fa-enter', function() {
+    store = $('#storeText').val();
+    $('#addStore').show();
+    $('#storeText').remove();
+    $('#faenter').remove();
+    makestore(store);
+  });
 
+// events
   $('#addStore').click(function() {
     $('#addStore').hide();
     $('#storeText').show();
@@ -26,41 +38,31 @@ var store,
     console.log(getInputBox());
   });
 
-  $('#fa-enter').click(function() {
-    store = $('#storeText').val();
-    $('#addStore').show();
-    $('#storeText').remove();
-    $('#faenter').remove();
-    makestore(store);
+  // $('#fa-enter').click(function() {
 
-    // $('.stores').on('click', '#'+storenospace+'check', function() {
-    //   $('.'+storenospace+'input').show();
-    //   $('#'+storenospace+'faenter').show();
-    //   $('#'+storenospace+'check').hide();
-    //   $('.'+storenospace+'input').focus();
-    // });
-    // $('.stores').on('click','#'+storenospace+'faenter', function(enter) {
-    //   $('.'+storenospace+'input').hide();
-    //   $('#'+storenospace+'faenter').hide();
-    //   $('#'+storenospace+'check').show();
-    //   var item = $('.'+storenospace+'input').val();
-    //   $('.'+storenospace+'input').val('');
-    //   console.log($('ul#'+storenospace));
-    //   $('ul#'+storenospace).append('<li><div class=\'item clearfix\'><i class=\"'+storenospace+' fa fa-check-square fa-3x\"></i><p>'+item+'</p><i id=\'#'+storenospace+'\' class=\"fa fa-minus-square fa-3x\"></i></div></li>');
-    // });
-    // $('#'+storenospace).on('click', '.fa-minus-square', function() {
-    //     $(this).parents('.item').remove();
-    // });
-    // $('#'+storenospace).on('click', '.'+storenospace+'.fa-check-square',function() {
-    //   console.log('click '+$(this).text());
-    //   $(this).parents('.item').toggleClass('check');
-    // });
-  });
 
-  // function addItem(item) {
-  //   var gitem = $('input').val();
-  //   console.log(store+item);
-  //   // $('#'+selectStore).append("<li><div class=\"item uncheck\"><i class=\"fa fa-check-square\"></i><p>"+gitem+"</p><i class=\"fa fa-minus-square\"></i></div></li>");
-  // }
+  //   // $('.stores').on('click', '#'+storenospace+'check', function() {
+  //   //   $('.'+storenospace+'input').show();
+  //   //   $('#'+storenospace+'faenter').show();
+  //   //   $('#'+storenospace+'check').hide();
+  //   //   $('.'+storenospace+'input').focus();
+  //   // });
+  //   // $('.stores').on('click','#'+storenospace+'faenter', function(enter) {
+  //   //   $('.'+storenospace+'input').hide();
+  //   //   $('#'+storenospace+'faenter').hide();
+  //   //   $('#'+storenospace+'check').show();
+  //   //   var item = $('.'+storenospace+'input').val();
+  //   //   $('.'+storenospace+'input').val('');
+  //   //   console.log($('ul#'+storenospace));
+  //   //   $('ul#'+storenospace).append('<li><div class=\'item clearfix\'><i class=\"'+storenospace+' fa fa-check-square fa-3x\"></i><p>'+item+'</p><i id=\'#'+storenospace+'\' class=\"fa fa-minus-square fa-3x\"></i></div></li>');
+  //   // });
+  //   // $('#'+storenospace).on('click', '.fa-minus-square', function() {
+  //   //     $(this).parents('.item').remove();
+  //   // });
+  //   // $('#'+storenospace).on('click', '.'+storenospace+'.fa-check-square',function() {
+  //   //   console.log('click '+$(this).text());
+  //   //   $(this).parents('.item').toggleClass('check');
+  //   // });
+  // });
 
 });
